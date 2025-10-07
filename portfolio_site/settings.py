ffc6d3fd8e29eee6a4e44d0dafc42621e96dc534
Dev <nameofdeev@gmail.com>
2025-10-07 17:40:08 +0545

    Added css and updated views, ,templates

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+
 ]
 
 MIDDLEWARE = [
@@ -106,9 +107,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  # points to your project-level static folder
+STATIC_ROOT = BASE_DIR / 'staticfiles'    # used for collectstatic in production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = ["127.0.0.1"]
