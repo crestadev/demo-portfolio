4803bc4221da6235bf981bfbf62050e363b00da3
Dev <nameofdeev@gmail.com>
2025-10-08 19:18:33 +0545

    Added testimonial and blogpost models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Testimonial(models.Model):
+    client_name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100, blank=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='testimonials/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.client_name} - {self.role}"
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    summary = models.TextField(max_length=300)
+    content = models.TextField()
+    featured_image = models.ImageField(upload_to='blog/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
