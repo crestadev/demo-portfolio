5c81a87d6184aef2c7be508b0f80b5b0bb739896
Dev <nameofdeev@gmail.com>
2025-10-08 18:52:49 +0545

    Added models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    technologies = models.CharField(max_length=300, help_text="Comma-separated techs")
+    demo_url = models.URLField(blank=True, null=True)
+    source_url = models.URLField(blank=True, null=True)
+    featured_image = models.ImageField(upload_to='projects/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class ProjectImage(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='projects/gallery/')
+    caption = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"{self.project.title} Image"
