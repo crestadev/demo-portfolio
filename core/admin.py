efb3c8597177647b52e84ae55dddbc54b07027bc
Dev <nameofdeev@gmail.com>
2025-10-08 18:53:32 +0545

    Updated admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Project, ProjectImage
 
-# Register your models here.
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ProjectImageInline]
+
+@admin.register(ProjectImage)
+class ProjectImageAdmin(admin.ModelAdmin):
+    list_display = ('project', 'caption')
