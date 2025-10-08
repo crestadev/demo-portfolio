7da4a30e039619257e7520812084c9ba5bde3488
Dev <nameofdeev@gmail.com>
2025-10-08 19:19:24 +0545

    Updated admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Testimonial, BlogPost
 
-# Register your models here.
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('client_name', 'role', 'created_at')
+    search_fields = ('client_name', 'role')
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'updated_at')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'summary')
