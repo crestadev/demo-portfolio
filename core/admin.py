2beba7499ba6b4d4a6ff436b812dcb087d803543
Dev <nameofdeev@gmail.com>
2025-10-08 19:01:51 +0545

    Rearranged models , admin and apps in new apps names projects while updating settings

--- core/admin.py
+++ core/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
-from .models import Project, ProjectImage
 
-class ProjectImageInline(admin.TabularInline):
-    model = ProjectImage
-    extra = 1
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
-    prepopulated_fields = {'slug': ('title',)}
-    inlines = [ProjectImageInline]
-
-@admin.register(ProjectImage)
-class ProjectImageAdmin(admin.ModelAdmin):
-    list_display = ('project', 'caption')
+# Register your models here.
