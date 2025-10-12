701152c1fba5791cfe47529a508e9250ce0a187f
Dev <nameofdeev@gmail.com>
2025-10-12 12:54:45 +0545

    Added views of blog_list and blog_detail

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
     path('projects/', include('projects.urls', namespace='projects')),
+    path('blog/', include('blog.urls', namespace='blog')),
+
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
