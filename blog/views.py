707fe227ad080e76bd2367b18451211f6ed47ebf
Dev <nameofdeev@gmail.com>
2025-10-12 13:13:07 +0545

    Added dropdown content

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import BlogPost
+import markdown
 
 def blog_list(request):
     posts = BlogPost.objects.order_by('-created_at')
@@ -7,4 +8,8 @@ def blog_list(request):
 
 def blog_detail(request, slug):
     post = get_object_or_404(BlogPost, slug=slug)
+    post.content_html = markdown.markdown(
+        post.content,
+        extensions=['fenced_code', 'codehilite', 'tables', 'toc']
+    )
     return render(request, 'blog/blog_detail.html', {'post': post})
