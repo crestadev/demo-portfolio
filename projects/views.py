e79b214e8f7d8fdb3c71f4f07539765d3b62d5c7
Dev <nameofdeev@gmail.com>
2025-10-15 16:12:21 +0545

    Updated views for better projects listingg

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Project
 
 def project_list(request):
-    projects = Project.objects.all()
-    return render(request, 'projects/project_list.html', {'projects': projects})
+    qs = Project.objects.all()
+    paginator = Paginator(qs, 6)  # 6 projects per page (tweak as desired)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'projects/project_list.html', {'page_obj': page_obj, 'projects': page_obj.object_list})
 
 def project_detail(request, slug):
     project = get_object_or_404(Project, slug=slug)
